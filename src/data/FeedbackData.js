84ae09a353975d69e3889d06436616b6f4cbf711
Elijah Cravens <elijahcravens46@gmail.com>
2024-02-07 13:40:14 -0700

    First commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 7,
+    text: 'lorem ipsum dolor sit amet 1'
+  },
+  {
+    id: 2,
+    rating: 8,
+    text: 'lorem ipsum dolor sit amet 2'
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: 'lorem ipsum dolor sit amet 3'
+  },
+]
+
+export default FeedbackData
